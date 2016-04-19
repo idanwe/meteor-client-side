d612c2a8f3ce1fd6e694065a4e0abdeabb912fb2
Idan Wender <idanwe2@gmail.com>
2016-04-19 22:47:51 +0300

    Fix setting custom DDP_DEFAULT_CONNECTION_URL

--- meteor-runtime-config.js
+++ meteor-runtime-config.js
@@ -2,7 +2,7 @@
   var _ = Package['underscore']._;
   var config = this.__meteor_runtime_config__ || {};
 
-  __meteor_runtime_config__ = _.extend({}, config, {
+  __meteor_runtime_config__ = _.defaults({}, config, {
     meteorEnv: {},
     DDP_DEFAULT_CONNECTION_URL: 'http://localhost:3000'
   });
